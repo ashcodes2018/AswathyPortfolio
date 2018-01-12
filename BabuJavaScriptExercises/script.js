d024f3de3f3a85069b747381e9779a27efea74e5
ashcodes2018 <35310785+ashcodes2018@users.noreply.github.com>
2018-01-12 00:39:57 -0500

    Add files via upload

---
+++ BabuJavaScriptExercises/script.js
@@ -0,0 +1,55 @@
+//question 1
+//var x="enter a number:";
+//var number=prompt(x);
+//
+//var sum=0;
+//for (var i = 0; i <= number; i++) {
+//	sum+=i;
+//}
+//alert(sum);
+//console.log(sum);
+
+
+//question 2
+//var play=prompt("do u want to play?");
+//var word;
+//var sentence=" ";
+//if(play==="yes"){
+//do{
+//	word=prompt("enter a word");
+//	sentence=sentence.concat(word," ");
+//	play=prompt("do you want to play?");
+//}
+//while(play==="yes");
+//}
+//console.log(sentence);
+
+
+
+//Question 3
+//var input = prompt("Would you like to print your name?");
+//if (input==="yes"){
+//var name = prompt("What is your name?");}
+//var output = "Hi my name is " + name;
+//while(input === "yes"){
+//console.log(output)
+//var input = prompt("Would you like to print this again?")
+//output = output + "!"
+//};
+
+
+
+//Question 4
+//var input=prompt("what time of the day is it?");
+//if(input==="morning"){
+//	console.log("Since its morning you should be eating breakfast.We suggest eggs and toast!");
+//}
+//else if(input==="noon"){
+//	console.log("Since its noon you should be eating lunch.We suggest a salad");
+//}
+//else if(input==="evening"){
+//	console.log("Since its evening you should be eating dinner.We suggest chicken and rice");
+//}
+//else{
+//	console.log("error");
+//}
